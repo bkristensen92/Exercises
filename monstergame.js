1e117cd9fef89adba905c2caf9cd507cc5d43252
bkristensen92 <40616639+bkristensen92@users.noreply.github.com>
2019-06-24 16:39:07 -0400

    Add files via upload

--- monstergame.js
+++ monstergame.js
@@ -6,8 +6,93 @@ let attack = document.getElementById("attack");
 let special = document.getElementById("special");
 let heal = document.getElementById("heal");
 let surrender = document.getElementById("surrender");
+let monsterAttack = ''
 
-attack.addEventListener('click' ,() =>{health.value -= 10});
-special.addEventListener('click' ,() => {health.value -= 15});
-heal.addEventListener('click' , () => {playerHealth.value += 10});
-surrender.addEventListener('click' , () => {alert("You have surrendered")});
+attack.addEventListener('click', () => {
+    attackClick();
+});
+
+function attackClick() {
+    attackFunction();
+    disableAttack();
+    checkWinCondition();
+}
+
+special.addEventListener('click', () => {
+    specialClick();
+});
+
+function specialClick() {
+    specialFunction();
+    disableSpecial();
+    checkWinCondition();
+}
+
+heal.addEventListener('click', () => {
+    healClick();
+});
+
+function healClick() {
+    healFunction();
+    disableHeal();
+}
+
+surrender.addEventListener('click', () => {
+    if (alert('You have surrendered, try again')) { }
+    else window.location.reload()
+});
+
+function attackFunction() {
+    health.value -= getDamage(1, 6);
+}
+
+function specialFunction() {
+    health.value -= getDamage(8, 13);
+}
+
+function healFunction() {
+    player_health.value += getHealth(3, 15);
+}
+
+function getDamage(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getHealth(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkWinCondition() {
+    if (health.value == 0) {
+        alert('You have saved Hyrule!!!');
+        window.location.reload();
+    }
+    else if (player_health.value == 0) {
+        alert('The kingdom of Hyrule is lost!!!');
+        window.location.reload();
+    }
+    return
+}
+
+function disableAttack() {
+    document.getElementById('attack').disabled = true;
+    setTimeout(function () { document.getElementById('attack').disabled = false; }, 1500);
+}
+
+function disableSpecial() {
+    document.getElementById('special').disabled = true;
+    setTimeout(function () { document.getElementById('special').disabled = false; }, 5000);
+}
+
+function disableHeal() {
+    document.getElementById('heal').disabled = true;
+    setTimeout(function () { document.getElementById('heal').disabled = false; }, 8000);
+}
+
+function getMonsterDamage() {
+
+}
+
+function monsterAttack() {
+
+}
