5609e187de3df7419d0c39c1d2ececf444c4b834
bkristensen92 <40616639+bkristensen92@users.noreply.github.com>
2019-07-10 23:12:35 -0400

    Add files via upload

--- Exercise12.js
+++ Exercise12.js
@@ -23,13 +23,13 @@ $(() => {
         { itemName: 'PrAna Stretch Zion Pants', cost: 59.82, count: 1, taxable: true },
         { itemName: 'Metolius Master Cams', cost: 269.78, count: 1, taxable: true },
     ]
-
+    
     let itemsInCart = [];
 
     $('.cancel_button').click(function () {
         if (confirm("Click OK to cancel transaction. Click cancel to continue.")) {
             $('.list_of_scanned_items').empty();
-            $('input').val('');
+            $('#input_number_of_items').val('');
             clearTotals();
             itemsInCart = [];
         }
@@ -48,35 +48,19 @@ $(() => {
     });
 
     $('.items').click(function () {
-        let itemID = this.id;
-        let cartIndex = $('#' + itemID).index();
-        let itemForSale = itemsForSale[cartIndex];
-        itemsInCart.push(itemForSale);
-        let subTotal = 0;
-        let taxTotal = 0;
-        let total = 0;
-        itemsInCart.forEach(function (item, i) {
-            let currentSubtotalCost = item.cost;
-            let currentTaxCost = item.taxable ? currentTaxRate * item.cost : 0;
-            subTotal = subTotal + currentSubtotalCost;
-            taxTotal = taxTotal + currentTaxCost;
-        });
-        $('.subtotal_price').html(`Subtotal : ` + '$' + subTotal.toFixed(2));
-
-        $('.list_of_scanned_items').append('<p class="listItem">' + $('#' + itemID).val() + '</p>');
-        if (itemForSale.taxable == true) {
-            $('.tax_price').html(`Tax : ` + '$' + taxTotal.toFixed(2));
-        }
-        total = subTotal + taxTotal;
-        $('.total_price').html(`Total : ` + '$' + total.toFixed(2));
-
-        $('.listItem').dblclick(function (element) {
-            if (confirm("Click OK to delete item. Click cancel to continue.")) {
-                element.target.remove();
+        let noOfItems = $('#' + 'input_number_of_items').val();
+        if(noOfItems > 0){
+            let i;
+            for(i = 0; i < noOfItems; i++)
+            {
+                pushItemIntoCart(this.id);
             }
-            //updatePrice();
-        })
-    })
+        }
+        else{
+            pushItemIntoCart(this.id);
+        }
+        $('#' + 'input_number_of_items').val(0);
+    });
 
     let currentTaxRate = .0725;
     $('.system_configuration_button').click(function () {
@@ -92,27 +76,69 @@ $(() => {
     $('.delete_key').click(function () {
         if (confirm("Click OK to delete last item. Click cancel to continue.")) {
             $('.list_of_scanned_items p').last().remove();
+            let itemID = $('.list_of_scanned_items p').last().id;
+            let elementIndex = $('#' + itemID).index();
+            $('#' + itemID).remove();
+            itemsInCart.splice(elementIndex, 1);
+            updatePrice('', true);
         }
     })
 
-
-
     $('.number_keys').click(function () {
-        $('#input').val($('#input').val() + $(this).val());
+        $('#input_number_of_items').val($('#input_number_of_items').val() + $(this).val());
 
     })
 
     $('.clear_key').click(function () {
-        $('input').val('');
+        $('#input_number_of_items').val('');
     })
 
-    function updatePrice() {
-
+    function pushItemIntoCart(id) {
+        let itemID = id;
+        let cartIndex = $('#' + itemID).index();
+        let itemForSale = itemsForSale[cartIndex];
+        itemsInCart.push(itemForSale);
+        let itemValue = $('#' + itemID).val();
+        updatePrice(itemValue, false);
+        
     };
+
+    function updatePrice(itemValue, isDelete) {
+        let subTotal = 0;
+        let taxTotal = 0;
+        let total = 0;
+        itemsInCart.forEach(function (item, i) {
+            let currentSubtotalCost = item.cost;
+            let currentTaxCost = item.taxable ? currentTaxRate * item.cost : 0;
+            subTotal = subTotal + currentSubtotalCost;
+            taxTotal = taxTotal + currentTaxCost;
+        });
+        if(!isDelete) {
+            let paragraphId = (itemsInCart.length + 1).toString() + 'listItemId';
+            $('.list_of_scanned_items').append('<p class="listItem" id="' + paragraphId + '">' + itemValue + '</p>');
+        }
+        $('.subtotal_price').html(`Subtotal : ` + '$' + subTotal.toFixed(2));
+        $('.tax_price').html(`Tax : ` + '$' + taxTotal.toFixed(2));
+        total = subTotal + taxTotal;
+        $('.total_price').html(`Total : ` + '$' + total.toFixed(2));
+    }
+
+        $('.list_of_scanned_items').dblclick(function (element) { 
+            if(element.target.className == 'listItem'){
+                console.log('#' + element.target.id);
+                let itemID = element.target.id;
+                let elementIndex = $('#' + itemID).index();
+                $('#' + itemID).remove();
+                itemsInCart.splice(elementIndex, 1);
+                console.log(elementIndex);
+                updatePrice('', true);
+            }
+        });
 
     function clearTotals() {
         $('.subtotal_price').html(`Subtotal : `);
         $('.tax_price').html(`Tax : `);
         $('.total_price').html(`Total : `);
     }
+
 });
